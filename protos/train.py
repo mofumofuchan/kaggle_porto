17646c2e0c02b08a3696bc02d28918455c559ca4
OKIMOTO Yusuke <you_oki1@yahoo.co.jp>
2018-11-21 16:55:36 +0000

    Add changes

--- protos/train.py
+++ protos/train.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import numpy as np
-from logging import StreamHandler, DEBUG, Formatter, FileHandler, getLogger
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import StratifiedKFold, ParameterGrid
+from sklearn.metrics import log_loss, roc_auc_score, roc_curve, auc
+
+from tqdm import tqdm
+from logging import StreamHandler, DEBUG, Formatter, FileHandler, getLogger
 
 from load_data import load_train_data, load_test_data
 
@@ -9,6 +13,13 @@ logger = getLogger(__name__)
 
 DIR = "result_tmp/"
 SAMPLE_SUBMIT_FILE = "../input/sample_submission.csv"
+
+
+def gini(y, pred):
+    fpr, tpr, thr = roc_curve(y, pred, pos_label=1)
+    g = 2 * auc(fpr, tpr) - 1
+    return g
+
 
 if __name__ == "__main__":
 
@@ -37,7 +48,48 @@ if __name__ == "__main__":
     
     logger.info("data preparation end {}".format(x_train.shape))
 
-    clf = LogisticRegression(random_state=0)
+    cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=0)
+    all_params = {"C": [10**i for i in range(-3, 4)],
+                  "fit_intercept": [True, False],
+                  "penalty": ["l2", "l1"],
+                  "random_state": [0]}
+
+    min_score = 100
+    min_params = None
+    for params in tqdm(list(ParameterGrid(param_grid=all_params))):
+        logger.info("params: {}".format(params))
+        list_logloss_score = []
+        list_gini_score = []
+        for train_idx, valid_idx in cv.split(x_train, y_train):
+            trn_x = x_train.iloc[train_idx, :]
+            val_x = x_train.iloc[valid_idx, :]
+
+            trn_y = y_train[train_idx]
+            val_y = y_train[valid_idx]
+
+            clf = LogisticRegression(**params)
+            clf.fit(trn_x, trn_y)
+
+            pred = clf.predict_proba(val_x)[:, 1]
+            sc_logloss = log_loss(val_y, pred)
+            sc_gini = gini(val_y, pred)
+
+            list_logloss_score.append(sc_logloss)
+            list_gini_score.append(sc_gini)
+            logger.debug("    logloss: {}, gini: {}".format(sc_logloss, sc_gini))
+
+            break  #FIXME
+
+        sc_logloss = np.mean(list_logloss_score)
+        sc_gini = - np.mean(list_gini_score)
+
+        logger.info("logloss: {}, gini: {}".format(sc_logloss, sc_gini))
+        if min_score > sc_gini:
+            min_score = sc_gini
+            min_params = params
+        logger.info("current minscore: {}, params: {}".format(min_score, min_params))
+
+    clf = LogisticRegression(**min_params)
     clf.fit(x_train, y_train)
 
     logger.info("train end")
@@ -47,7 +99,7 @@ if __name__ == "__main__":
     x_test = df[use_cols].sort_values("id")
 
     logger.info("test data load end {}".format(x_test.shape))
-    pred_test = clf.predict_proba(df)
+    pred_test = clf.predict_proba(df)[:, 1]
 
     df_submit = pd.read_csv(SAMPLE_SUBMIT_FILE).sort_values("id")
     df_submit["target"] = pred_test
